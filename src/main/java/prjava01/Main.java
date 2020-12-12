97119519202d68533faec749c86bd843e440cbba
pecoti2021daw2@protonmail.com <>
2020-12-12 20:20:02 +0100

    Versió 0.2 del projecte prjava01

--- src/main/java/prjava01/Main.java
+++ src/main/java/prjava01/Main.java
@@ -22,6 +22,7 @@ public class Main {
  public static void main(String[] args) throws IOException {
     File f = new File("fitxer.html");
     try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+        bw.write(" <h1>DAW2 m08uf4eac1</h1>");bw.newLine(); 
         bw.write("<html>");bw.newLine();
         bw.write(" <head>");bw.newLine();
         bw.write(" <title>");bw.newLine();
